1e1e829c88b49d669652ab9f4f78fb8b4ca5a785
Victorino Machava <vitomachava@gmail.com>
2016-11-13 16:39:56 +0200

    set up ESLint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in development mode...'));
+console.log(chalk.green('Starting app in development mode...')); // eslint-disable-line no-console
